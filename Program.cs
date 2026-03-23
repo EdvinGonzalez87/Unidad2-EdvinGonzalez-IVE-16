5a35636581a17e6c005f3bc899761f6cbf16635a
EdvinGonzalez87 <agonzalezdb23@gmail.com>
2026-03-23 11:52:54 -0600

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -2,28 +2,20 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("===Mensaje de voz===");
-        int opc;
-        Console.WriteLine("1-Menu Principal\n");
-        Console.WriteLine("2-Vigilancia\n");
-        Console.WriteLine("3-Mantenimiento\n");
-        Console.WriteLine("4-Salir\n");
-        Console.WriteLine("Digite una opcion [ ] ");
-        opc = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("===Promedio de notas");
+        int n1,n2, n3, n4, n5;
+        string materia;
+        Console.WriteLine("Digite la materia:");
+        materia =Console.ReadLine();
+        Console.WriteLine("Digite 5 notas: ");
+        n1=Convert.ToInt32(Console.ReadLine());
+        n2 = Convert.ToInt32(Console.ReadLine());
+        n3 = Convert.ToInt32(Console.ReadLine());
+        n4 = Convert.ToInt32(Console.ReadLine());
+        n5 = Convert.ToInt32(Console.ReadLine());
 
-        if (opc == 1) 
-        {
-            Console.Clear();
-            Console.WriteLine("Bienvenido al Depto. de recepccion");
-        }
-        if (opc == 2)
-            Console.WriteLine("Bienvenido al Depto. de Vigilancia");
-        if (opc == 3)
-            Console.WriteLine("Bienvenido al Depto. de Mantenimiento");
-        if (opc != 1 && opc!= 2 && opc!= 3)
-            Console.WriteLine("Saliendo del sistema");
-
-        Console.Clear();
-
+        double promedio = (n1+n2+n3+n4+n5)/5;
+        Console.WriteLine("La Materia:" + materia);
+        Console.WriteLine("El Promedio:" + promedio);
     }
 }
