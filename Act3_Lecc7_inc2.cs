d5127a733234e4613b62c99354bbf6749506cb77
EdvinGonzalez87 <agonzalezdb23@gmail.com>
2026-04-06 17:37:44 -0600

    Add files via upload

---
+++ Act3_Lecc7_inc2.cs
@@ -0,0 +1,23 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        double l1 = double.Parse(Console.ReadLine());
+        double l2 = double.Parse(Console.ReadLine());
+        double l3 = double.Parse(Console.ReadLine());
+
+        if (l1 == l2 && l2 == l3)
+        {
+            Console.WriteLine("El triángulo es: Equilátero");
+        }
+        else if (l1 == l2 || l1 == l3 || l2 == l3)
+        {
+            Console.WriteLine("El triángulo es: Isósceles");
+        }
+        else
+        {
+            Console.WriteLine("El triángulo es: Escaleno");
+        }
+    }
+}
