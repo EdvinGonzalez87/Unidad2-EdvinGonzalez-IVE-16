4b1e50942e7f205de30807f533c63ac4fdb4078c
EdvinGonzalez87 <agonzalezdb23@gmail.com>
2026-04-06 17:42:16 -0600

    Add files via upload

---
+++ Act2_Lecc20_inc1.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        string clave = Console.ReadLine();
+
+        if (clave == "Password123")
+        {
+            Console.WriteLine("Bienvenida");
+        }
+        else
+        {
+            Console.WriteLine("El ingreso está prohibido");
+        }
+
+    }
+}
